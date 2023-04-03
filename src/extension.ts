b56255e9ad334c3c95bb84910dc73902d0e7cb23
George Looshch <looshch@loosh.ch>
2023-04-03 12:09:28 +0100

    Add auto-update on start.

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,6 @@
 import * as vscode from 'vscode';
 import * as cp from 'child_process';
 
-const installMissing = (cb: () => void) => {
-  cp.exec('whereis -q gouse', (err, stdout) => {
-    if (err) return vscode.window.showErrorMessage(err.message);
-    if (stdout) return cb();
-    vscode.window.showErrorMessage('gouse is not installed.', 'Install')
-      .then(install => {
-        if (!install) return;
-        cp.exec('go install github.com/looshch/gouse@latest', err => {
-          if (err) return vscode.window.showErrorMessage(err.message);
-          cb();
-        });
-      });
-  });
-}
-
 const toggle = () => {
   const editor = vscode.window.activeTextEditor;
   if (!editor) return;
@@ -24,11 +9,13 @@ const toggle = () => {
     .then(() => cp.exec('gouse -w ' + editor.document.uri.fsPath, err => {
       const msg = err?.message;
       if (!msg) return;
-      if (msg.includes('command not found')) return installMissing(toggle);
       vscode.window.showErrorMessage(msg);
     }));
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(vscode.commands.registerCommand('gouse.toggle', toggle));
+  cp.exec('go install github.com/looshch/gouse@latest', err => {
+    context.subscriptions.push(vscode.commands.registerCommand('gouse.toggle', toggle));
+    if (err) return vscode.window.showErrorMessage(err.message);
+  });
 }
